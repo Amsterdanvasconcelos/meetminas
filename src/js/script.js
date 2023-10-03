667b87933c21d32f1890e06c517546406d8d4ea4
Amsterdan Vasconcelos <135554841+Amsterdanvasconcelos@users.noreply.github.com>
2023-10-03 04:17:59 -0300

    add js of the show mwnu

--- src/js/script.js
+++ src/js/script.js
@@ -46,8 +46,8 @@ const _elements = {
 	date: document.querySelector(".date"),
 
 	scrollLinks: document.querySelectorAll('[data-link="skroll"]'),
-	navbarList: document.querySelector(".navbar-list"),
-	toggle: document.querySelector(".navbar-header__toggle"),
+	navbar: document.querySelector('[data-menu="navbar"]'),
+	btnMenu: document.querySelector('[data-menu="btn-menu"]'),
 
 	galleryItems: document.querySelectorAll(".galeria-item"),
 	sliderThumbsImage: document.querySelectorAll(".slider-thumbs__img"),
@@ -68,7 +68,10 @@ _elements.scrollLinks.forEach(link => {
 	link.addEventListener('click', event => {
 		const id = link.getAttribute('href')
 		const element = document.querySelector(id)
-		const position = element.offsetTop
+		const position = element.offsetTop - 72
+
+		// Fecha a navbar-mobile após o click
+		_elements.navbar.classList.remove('c-navbar--show-links')
 		
 		window.scrollTo({
 			top: position,
@@ -79,8 +82,8 @@ _elements.scrollLinks.forEach(link => {
 	})
 });
 
-_elements.toggle.addEventListener("click", () => {
-
+_elements.btnMenu.addEventListener("click", () => {
+	_elements.navbar.classList.toggle('c-navbar--show-links')
 });
 
 _elements.galleryItems.forEach(item => {
